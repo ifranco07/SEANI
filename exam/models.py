6108e4bfb39186bd97ee18f2025ce03cead69f59
ifranco07 <i.francomtz07@gmail.com>
2024-02-29 18:00:24 -0600

    SEANI2

--- exam/models.py
+++ exam/models.py
@@ -1,28 +1,90 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-class Stage (models.Model):
-    stage= models.IntegerField(
-        verbose_name= 'Etapa'
+from career.models import Career
+from library.models import Module, Question
+
+class Stage(models.Model):
+    stage = models.IntegerField(
+        verbose_name = "Etapa"
     )
-    applicayion_date= models.DateField(
-        verbose_name= 'Fecha de Aplicación'
+    application_date = models.DateField(
+        verbose_name = "Fecha de Aplicacion"
     )
     
     @property
     def year(self):
         return self.application_date.year
-
+    
     @property
     def month(self):
-     months = ['enero', 'febrero', 'marzo','abirl', 'mayo', 'junio',
-               'julio', 'agosto', 'septiembre','octubre', 'noviembre', 'diciembre',]
-     return months[self.application_date.month - 1]
-
-    def __str__(self):
+        months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+        return months[self.application_date.month -1]
+    
+    def _str_(self):
         return f"{self.stage} - {self.month}{self.year}"
+    
+    class Meta:
+        verbose_name = "etapa"
+        verbose_name_plural = "etapas"
+        
+class Exam(models.Model):
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE,
+        verbose_name = "Usuario")
+    career = models.ForeignKey(
+        Career,
+        on_delete=models.CASCADE,
+        verbose_name = "Carrera")
+    stage = models.ForeignKey(
+        Stage, 
+        on_delete=models.CASCADE,
+        verbose_name = "Etapa")
+    modules = models.ManyToManyField(
+            Module,  
+            through='ExamModule')
+    questions = models.ManyToManyField(
+        Question,
+        through='Breakdown')
+    score = models.FloatField(
+        verbose_name = "Calificacion",
+        default = 0.0)
+    created = models.DateTimeField(
+        verbose_name = "Fecha de Creacion",
+        auto_now_add=True)
+    update = models.DateField(
+        auto_now=True,
+        verbose_name = "Fecha de actualizacion")
+   
+    def set_modules(self):
+       for module in Module.objects.all():
+           self.modules.add(module)
+   
+    def set_questions(self):
+        for module in self.modules.all():
+            for question in module.question_set.all():
+                # self.questions.add(question)
+                Breakdown.objects.create(
+                    exam= self,
+                    question= question,
+                    correct = question.correct)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.score}"
+    
+    class Meta:
+        verbose_name = "Examen"
+        verbose_name_plural = "Examenes"
 
-    class Meta: 
-        verbose_name = 'etapa'
-        verbose_name_plural = 'etapas'
+class ExamModule(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    active = models.BooleanField(default = True)
+    score = models.FloatField(default=0.0)
 
+class Breakdown (models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=5, default= '-')
+    correct = models.CharField(max_length=5, default= '-')
--- exam/views.py
+++ exam/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Stage, Exam
+from django.contrib.auth.models import User
+from career.models import Career
 # Create your views here.
+def create (request):
+    stage = Stage.objects.get(pk=5)
+    career = Career.objects.get(pk=1)
+    user = User.objects.create(
+                username= 'peta',
+                password= 'peta')
+    exam = Exam.objects.create(user= user,
+                            stage= stage,
+                            career= career)    
+    exam.set_modules()
+    exam.set_questions()
+    return HttpResponse('Usuario y examen creado')
